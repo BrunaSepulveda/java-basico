51c466be2b384d54a51fc75ead19eb7d0cedde1f
BrunaSepulveda <sepulvedabruna@gmail.com>
2023-04-14 16:19:43 -0300

    feat donw e up casting

---
+++ src/entities/UpDownCasting.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class UpDownCasting {
+  
+  static void test(){
+    Account acc = new Account();
+    BusinessAccount bacc = new BusinessAccount();
+    SavingsAccount sacc = new SavingsAccount();
+    //up perde suas especificações
+    Account acc1 = bacc;
+
+    // donw
+    BusinessAccount bacc2 = (BusinessAccount) acc;
+
+    /*
+     Erro em tempo de execução
+     BusinessAccount bacc3 = (BusinessAccount) sacc;
+     instanceof checa a classe
+    */
+  }
+}
