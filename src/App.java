10457dbbc6559354119dfa778c3f8a3273e41275
BrunaSepulveda <sepulvedabruna@gmail.com>
2023-04-14 15:58:04 -0300

    feat: exemplo de uso protected

--- src/App.java
+++ src/App.java
@@ -1,12 +1,5 @@
-import java.time.LocalDate;
 import java.util.Locale;
 import java.util.Scanner;
-
-import entities.DateTest;
-import entities.Department;
-import entities.HourContract;
-import entities.Worker;
-import entities.enums.WorkerLevel;
 public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
