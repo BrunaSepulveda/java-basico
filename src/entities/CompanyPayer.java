1f77532ef162ac7af9d645a52dff2e6575334f18
BrunaSepulveda <sepulvedabruna@gmail.com>
2023-04-24 17:33:49 -0300

    feat: inicia exercicio de fixação de herança

---
+++ src/entities/CompanyPayer.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class CompanyPayer extends TaxPayer {
+
+  @Override
+  public double tax() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'tax'");
+  }
+  
+}
